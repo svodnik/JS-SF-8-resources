90538792cf1549bac94dabff672b207040e0fd00
Sasha Vodnik <sv@sashavodnik.com>
2017-09-18 18:02:01 -0700

    swapped out correct files for starter code

---
+++ 03-conditionals-functions/starter-code/1-exponent-lab/app.js
@@ -0,0 +1,14 @@
+/*
+Write code that creates a for loop that calculates 2 to a given power, and console.logs each step of the calculation. For instance, when calculating 2 to the 10th power (2^10), the for loop should console.log the result of 2^1, then the result of 2^2, etc., until it gets to 2^10.
+
+Start by creating a variable to store the power of 2 you want to calculate. (In the above example, the value of this variable would be 10.)
+
+Then create a for loop to calculate each step of this process and log it to the console.
+
+BONUS 1: Rewrite your code to allow a user to enter the exponent value, rather than hard-coding it into your program. (Hint: Read up on the window.prompt method at https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt.)
+
+BONUS 2: Rewrite your code to use a while loop rather than a for loop. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while)
+
+BONUS 3: Rewrite your code to use a do/while loop rather than a for loop or while loop. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/do...while)
+*/
+
--- 03-conditionals-functions/starter-code/3-ages-lab/app.js
+++ 03-conditionals-functions/starter-code/3-ages-lab/app.js
@@ -1,6 +1,5 @@
 /*
-    When programming user interfaces, you will often need to display results based on a certain input. In this exercise, help the students design
-    a program that will let users know what legal privileges U.S. citizens enjoy based on their age.
+    When programming user interfaces, you will often need to display results based on a certain input. In this exercise, you will design a program that will let users know what legal privileges people in the U.S. enjoy based on their age.
 
     Write a program that outputs results based on users’ age. This exercise draws on if/else statements, Boolean logic, and comparison operators. 
     See the conditions below:
@@ -14,4 +13,8 @@
     - If you are 62 or older, you collect social security benefits
 
     The results that your program provides should be only the most recent privilege based on the provided age.
+
+    BONUS 1: Rewrite your code to allow a user to enter an age value, rather than hard-coding it into your program. (Hint: Read up on the window.prompt method at https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt.)
+
+    BONUS 2: Rewrite your code to use a switch statement rather than if and else statements. (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch)
 */
